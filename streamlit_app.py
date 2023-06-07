674edfa0123754f9886358b6daaa758d18d28925
Radhe-77 <135818196+Radhe-77@users.noreply.github.com>
2023-06-07 16:11:38 +0530

    Update streamlit_app.py
    Menu title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner'
+streamlit.title('My Parents New Healthy Diner')
                
-               'Breakfast Menu')
+streamlit.subtitle('Breakfast Menu')
 
 
